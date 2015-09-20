46500e7d074a8ad50edd411f8eab68b26a637fa3
黄莉 <yatoufe@163.com>
2015-09-20 11:08:15 +0800

    用户，角色，资源 管理 相关代码 编写

--- routes/security.js
+++ routes/security.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var http = require('./../lib/http-utils-defult');
-//var http = require('./../lib/http-utils')({
-//    hostname: '192.168.0.106',
-//    port: 8080,
-//    headers: {
-//        'Authorization': 'Basic YWRtaW46MTIzNDU2'
-//    }
-//});
+var async = require('async');
+
 var router = express.Router();
 
 router.get('/menus', function (req, res, next) {
@@ -40,7 +35,7 @@ router.post('/menus/save', function (req, res, next) {
         });
     }
 });
-//�û��б��ѯ
+//用户列表查询
 router.get('/users', function (req, res, next) {
     http.get('/security/users', function (_res) {
         _res.on('complete', function (data) {
@@ -48,23 +43,50 @@ router.get('/users', function (req, res, next) {
         });
     });
 });
-//�û����
+//用户新增
 router.get('/users/add', function (req, res, next) {
-    http.get('/security/users', function (_res) {
+    async.parallel({
+        roles: function (callback) {
+            http.get('/security/roles', function (_res) {
+                _res.on('complete', function (data) {
+                    callback(null, data.pageItems);
+                });
+            });
+        },
+        websites: function (callback) {
+            http.get('/system/websites', function (_res) {
+                _res.on('complete', function (data) {
+                    callback(null, data);
+                });
+            });
+        }
+    }, function (err, results) {
+        res.render('security/users_add', results);
+    });
+
+
+});
+//用户修改
+router.get('/users/edit', function (req, res, next) {
+    http.get('/security/roles', function (_res) {
         _res.on('complete', function (data) {
-            res.render('security/users_add');
+            res.render('security/users_edit', {roles: data.pageItems});
+        });
+    });
+    http.get('/security/roles', function (_res) {
+        _res.on('complete', function (data) {
         });
     });
 });
-
+//角色列表
 router.get('/roles', function (req, res, next) {
     http.get('/security/roles', function (_res) {
         _res.on('complete', function (data) {
-            res.render('security/roles',{pager:data});
+            res.render('security/roles', {pager: data});
         });
     });
 });
-
+//角色查询
 router.post('/users/search', function (req, res, next) {
     http.get('/security/users', function (_res) {
         _res.on('complete', function (data) {
@@ -73,68 +95,108 @@ router.post('/users/search', function (req, res, next) {
     });
 });
 
+
+//用户保存
 router.post('/users/save', function (req, res, next) {
-     http.post('/security/users', req.body, function (_res) {
+    http.post('/security/users', req.body, function (_res) {
+        _res.on('complete', function (data) {
+            console.log(data)
+            res.json(data);
+        });
+    });
+});
+//用户删除
+router.post('/users/:id/delete', function (req, res, next) {
+    http.delete('/security/users/' + req.params.id, function (_res) {
+        _res.on('complete', function (data) {
+            res.json(data);
+            console.log(data);
+        });
+    });
+});
+
+
+//角色添加跳转页面
+router.get('/roles/add', function (req, res, next) {
+    res.render('security/roles_add');
+});
+//角色编辑
+router.get('/roles/:code/edit', function (req, res, next) {
+    http.get('/security/roles/' + req.params.code, function (_res) {
+        _res.on('complete', function (data) {
+            res.render('security/roles_edit', {role: data});
+            console.log(data);
+        });
+    });
+});
+//角色保存
+router.post('/roles/save', function (req, res, next) {
+    if(!!req.body.code) {
+        http.put('/security/roles/'+req.body.code, req.body, function (_res) {
             _res.on('complete', function (data) {
-                console.log(data)
                 res.json(data);
             });
         });
-});
-
-/**
- * �༭
- */
-router.get('/users/edit', function (req, res, next) {
-    http.get('/security/users/'+req.query.category,  function (_res) {
-        _res.on('complete', function (data) {
-            res.render('security/users_edit', {category: data});
+    }else{
+        http.post('/security/roles', req.body, function (_res) {
+            _res.on('complete', function (data) {
+                res.json(data);
+            });
         });
-    });
+    }
 });
-/**
- * ɾ��
- */
-router.post('/users/delete', function (req, res, next) {
-    http.delete('/security/users/'+req.body.ids, function (_res) {
-        _res.on('complete', function (data) {
-            res.json(data);
-        });
-    });
-});
-
-router.get('/roles/add',function(req, res, next){
-    res.render('security/roles_add');
-});
-router.get('/roles/:code/edit',function(req, res, next){
-    http.get('/security/roles/'+req.params.code, function (_res) {
-        _res.on('complete', function (data) {
-            res.render('security/roles_edit',{role:data});
-        });
-    });
-});
-
-router.post('/roles/save', function (req, res, next) {
-    http.post('/security/roles',req.body, function (_res) {
-        _res.on('complete', function (data) {
-            res.json(data);
-        });
-    });
-});
-
+//角色查询
 router.post('/roles/search', function (req, res, next) {
-    http.get('/security/roles',req.params, function (_res) {
+    http.get('/security/roles', req.params, function (_res) {
         _res.on('complete', function (data) {
             res.json(data);
         });
     });
 });
-
+//角色删除
 router.post('/roles/delete', function (req, res, next) {
     console.log(req.body.codes)
-    http.delete('/security/roles',req.body.codes, function (_res) {
+    http.delete('/security/roles', req.body.codes, function (_res) {
         _res.on('complete', function (data) {
             res.json(data);
+        });
+    });
+});
+
+//资源相关
+router.get('/resources', function (req, res, next) {
+    http.get('/security/resources', function (_res) {
+        _res.on('complete', function (data) {
+            res.render('security/resources', {pager: data});
+        });
+    });
+});
+
+//资源添加跳转页面
+router.get('/resources/add', function (req, res, next) {
+    res.render('security/resources_add');
+});
+//保存
+router.post('/resources/save', function (req, res, next) {
+    http.post('/security/resources', req.body, function (_res) {
+        _res.on('complete', function (data) {
+            res.json(data);
+        });
+    });
+});
+//查询
+router.post('/resources/search', function (req, res, next) {
+    http.get('/security/resources', function (_res) {
+        _res.on('complete', function (data) {
+            res.json(data);
+        });
+    });
+});
+//编辑
+router.get('/resources/:id/edit', function (req, res, next) {
+    http.get('/security/resources/' + req.params.code, function (_res) {
+        _res.on('complete', function (data) {
+            res.render('security/resources_edit', {resource: data});
         });
     });
 });
